5eb1ac40eb1ebcc683302b4098c64e121798ef8e
Rom Grk <romgrk.cc@gmail.com>
2016-05-14 21:35:44 -0400

    up.

--- src/neovim/input.ts
+++ src/neovim/input.ts
@@ -195,7 +195,7 @@ export default class NeovimInput {
         this.element.addEventListener('compositionend', this.endComposition.bind(this));
         this.element.addEventListener('keydown', this.onInputNonText.bind(this));
         this.element.addEventListener('input', this.onInputText.bind(this));
-        this.element.addEventListener('blur', this.onBlur.bind(this));
+        //this.element.addEventListener('blur', this.onBlur.bind(this));
         this.element.addEventListener('focus', this.onFocus.bind(this));
         this.store.on('cursor', this.updateElementPos.bind(this));
 
